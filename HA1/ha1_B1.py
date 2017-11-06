8bf4845b4cfddf765fa81a9b4b6f9e547b3ab213
elt12ddo <elt12ddo@student.lu.se>
2017-11-06 14:49:47 +0100

    Now with file support

--- HA1/ha1_B1.py
+++ HA1/ha1_B1.py
@@ -29,7 +29,10 @@ def findDigit(str):
         temp = sum
             
         
-a = ["12774212857X4109", "586604X108627571", "7473X86953606632", "4026467X45830632", "20X3092648604969"]
+
+file = open("B1.txt","r")
+a = file.read().splitlines()
+file.close()
 print("Numbers:")
 for i in range(len(a)):
     print(a[i])
