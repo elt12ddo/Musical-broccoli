95dc601be4763577abc08f2d998f253f2f42579a
elt12ddo <elt12ddo@student.lu.se>
2017-11-06 13:45:13 +0100

    B1 done

--- HA1/ha1.py
+++
@@ -1,8 +0,0 @@
-# -*- coding: utf-8 -*-
-"""
-Created on Mon Nov  6 10:57:29 2017
-
-@author: Daniel
-"""
-
-print("A file to make git add the folder...")
---
+++ HA1/ha1_B1.py
@@ -0,0 +1,49 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Nov  6 10:57:29 2017
+
+@author: Daniel
+"""
+
+
+
+
+
+def findXpos(str):
+    for i in range(len(str)):
+        if(str[i:i+1] == "X"):
+            return i
+    return None
+
+def findDigit(str):
+    sum = 0
+    mult = 1
+    factor = 0
+    for i in reversed(range(len(str))):
+        if str[i:i+1] == "X":
+            factor=mult
+        if str[i:i+1] != "X":
+            temp = int(str[i:i+1])*mult
+            if temp > 9:
+                temp = temp - 9
+            sum = sum + temp
+        mult = mult%2 + 1
+        #print("Sum: "+repr(sum)+", mult: "+repr(mult)+", factor: "+repr(factor))
+    temp = sum
+    for j in range(0,10):
+        term = j*factor
+        if term > 9:
+            term= term - 9
+        temp = temp + term
+        if temp%10 == 0:
+           return j
+        temp = sum
+            
+
+
+a = ["12774212857X4109", "586604X108627571", "7473X86953606632", "4026467X45830632", "20X3092648604969"]
+for i in range(len(a)):
+    print(a[i])
+print("****************")
+for i in range(len(a)):
+    print(findDigit(a[i]), end="")
