1ce34c286e3fdeb14d57ee529228ea9d6f60af05
cart92 <david.cartbo@gmail.com>
2017-11-07 20:43:30 +0100

    MIcromint is done and works automagically

---
+++ HA1/Micromint.py
@@ -0,0 +1,34 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Nov  7 15:22:42 2017
+
+@author: David Cartbo
+"""
+
+import array
+import random
+import numpy
+import time
+
+def MicroMint(u, k, c):
+    u = (2**u)
+    a = array.array('i',(0 for i in range(0,u)))
+    t = 0
+    while(c > 0):
+        n = random.randrange(u)
+        a[n] += 1
+        if(a[n] == k):
+            c -= 1
+        t += 1
+    return t
+
+def MicroMint2(c, u, k, width):
+    start_time = time.time()
+    results = []
+    for y in range(0,10): # Make sure we have a decent amount of data to be confident that std is not 0 (and it saves a few cycles in most cases too as there is no evalution here)
+        results.append(MicroMint(u, k, c))
+    while((7.32 * numpy.std(results) / (len(results)**0.5)) > width):
+        results.append(MicroMint(u, k, c))
+    print("----- width: %s -----" % (7.32 * numpy.std(results) / (len(results)**0.5)))
+    print("----- %s seconds -----" % (time.time() - start_time))
+    return numpy.mean(results)
