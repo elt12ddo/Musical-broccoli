571d100fd5069ad5097cebba150d95a510f01ccc
Cart92 <david.cartbo@gmail.com>
2017-12-17 16:34:03 +0100

    B3 is now working

--- HA5/ha5_B3.py
+++ HA5/ha5_B3.py
@@ -4,10 +4,27 @@ Created on Fri Dec 15 16:30:30 2017
 
 @author: David Cartbo
 """
-import sys
 import math
+import base64
 
-def DER_encode(a):
+#As described on Wikipedia
+def invmod(a,n):
+    t, t1 = 1, 0
+    r, r1 = a, n
+    while r1:
+        q = r // r1
+        t, t1 = t1, t - q * t1
+        r, r1 = r1, r - q * r1
+    if(r > 1):
+        raise Exception('modular inverse does not exist')
+    if(t < 0):
+        t += n
+    return t
+
+def lcm(a,b):
+    return abs(a*b)//math.gcd(a,b)
+
+def DER_encode_INT(a):
     data = hex(a)[2:]
     
     #begin magic
@@ -18,7 +35,6 @@ def DER_encode(a):
 
     if(len(data) % 2 == 1):
         data = '0' + data
-    print("IT WILL NOT WORK FOR EVERYTHING (YES IT WILL!)")
     return '02' + getLengthString(len(data)//2) + data
     
 def getLengthString(a):
@@ -34,7 +50,20 @@ def getLengthString(a):
     length = 128+(len(string)//2)
     return hex(length)[2:] + string
 
-#The 2-complement is a bit off
-#https://lapo.it/asn1js/#028180E6802D70D04DEA6724D9398116CBDAD4A7D824A6846432D8CAE83EA9BE7027F7BF80082FD9B6ED5A36656DC92E1141290C9437FEC219F981C255599849E1B65C64D4184A96F4C0AF287577997BE19CF500A7997F2E362A2EF263E75AF3BE6611DED3D18E5E9C02AAEE4484593017806531D9A9507C2241806E7CFA7298C9DBCD
-#print(DER_encode(161863091426469985001358176493540241719547661391527305133576978132107887717901972545655469921112454527920502763568908799229786534949082469136818503316047702610019730504769581772016806386178260077157969035841180863069299401978140025225333279044855057641079117234814239380100022886557142183337228046784055073741))
-print(DER_encode(10000))
+def DER_encode_SEQ(p, q, e):
+    n = p*q
+    #lambda_n = lcm(p-1,q-1) # Apparantly the given values was longer than necessary
+    lambda_n = (p-1)*(q-1)
+    d = invmod(e,lambda_n)
+    exponent1 = d % (p-1)
+    exponent2 = d % (q-1)
+    coefficient = invmod(q,p)
+    
+    string = DER_encode_INT(0) + DER_encode_INT(n) + DER_encode_INT(e) + DER_encode_INT(d) + DER_encode_INT(p) + DER_encode_INT(q) + DER_encode_INT(exponent1) + DER_encode_INT(exponent2) + DER_encode_INT(coefficient)
+    string = '30' + getLengthString(len(string)//2) + string
+
+    return base64.b64encode(bytearray.fromhex(string))
+
+#print(DER_encode_SEQ(2530368937,2612592767,65537))
+    
+print(DER_encode_SEQ(139721121696950524826588106850589277149201407609721772094240512732263435522747938311240453050931930261483801083660740974606647762343797901776568952627044034430252415109426271529273025919247232149498325412099418785867055970264559033471714066901728022294156913563009971882292507967574638004022912842160046962763,141482624370070397331659016840167171669762175617573550670131965177212458081250216130985545188965601581445995499595853199665045326236858265192627970970480636850683227427420000655754305398076045013588894161738893242561531526805416653594689480170103763171879023351810966896841177322118521251310975456956247827719,65537))
